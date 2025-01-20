525aa0cb5ab58688a2ad9a852eebe68e0d267114
shivamsingh17 <singhshivam635@gmail.com>
2025-01-20 20:43:31 +0530

    coding round submission

--- src/main/java/com/example/watchlistms/service/WatchlistService.java
+++ src/main/java/com/example/watchlistms/service/WatchlistService.java
@@ -1,14 +1,15 @@
 package com.example.watchlistms.service;
 
 import com.example.watchlistms.dto.WatchlistResponse;
-import com.example.watchlistms.repository.WatchlistRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class WatchlistService implements WatchlistServiceInterface {
 
-    WatchlistRepository watchlistRepository;
+//    WatchlistRepository watchlistRepository;
     /*
     Function to return Waltchlist
     @param userId - String
@@ -17,7 +18,16 @@ public class WatchlistService implements WatchlistServiceInterface {
      */
     public WatchlistResponse addToWatchlist(String userId, String movieId) {
 
-        WatchlistResponse watchlistResponse = watchlistRepository.findMovie(userId, movieId);
-        return watchlistResponse;
+//        WatchlistResponse watchlistResponse = watchlistRepository.findMovie(userId, movieId);
+        WebClient webClient = WebClient.builder()
+                .baseUrl("http://34.148.175.45:8080/api-gateway/movie-service")
+                .build();
+
+        return webClient.get()
+                .uri("//movie/{id}", movieId)
+                .header(HttpHeaders.AUTHORIZATION, "X-API-Key: movie-service6cda51ffd6384878af98797672977a0f")
+                .retrieve()
+                .bodyToMono(Product[].class);
+
     }
 }
